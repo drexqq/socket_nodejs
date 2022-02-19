d0448dc881e8cdfc42c327fe7cb13abdd1730f2a
drexqq <01037923530a@gmail.com>
2022-02-19 17:18:20 +0900

    [FEAT] socket.io를 활용한 채팅서버

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const port = 3000;
 const app = require("express")();
 const http = require("http").createServer(app);
-const port = 3000;
-
+const io = require('socket.io')(http);
 
 app.get("/", (req, res) => {
-	res.send("hello socket js");
+	res.sendFile(__dirname + "/index.html");
+})
+
+io.on("connection", (socket) => {
+	socket.on("request_message", (msg) => {
+		io.emit("response_message" , msg);
+	})
+
+	socket.on("disconnect", async () => {
+		console.log("disconnected");
+	})
 })
 
 http.listen(port, () => {
